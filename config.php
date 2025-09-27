0895c3cca10802e662c1d74c81fee7cbfa9b4c49
Jaydunmoree <jomarsison1020@gmail.com>
2025-09-28 02:26:18 +0800

    Use environment variables for DB connection

--- config.php
+++ config.php
@@ -1,5 +1,9 @@
 <?php
-$db = mysqli_connect("localhost", "root", "", "todo")
-    or
-    die("Connection failed: " . mysqli_connect_error());
+$db_host = getenv('DB_HOST');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
+$db_name = getenv('DB_NAME');
+
+$db = mysqli_connect($db_host, $db_user, $db_pass, $db_name)
+    or die("Connection failed: " . mysqli_connect_error());
 ?>
